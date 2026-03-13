4178651c83ba4fc35e1de986417acb46001f3c90
Dacosta <324services.cls@gmail.com>
2026-03-13 15:24:07 +0000

    Prepare for render deployment

--- popo_barbershop/settings.py
+++ popo_barbershop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d@!^)pqfb9$lq3x7ee&_*d2%i1m2lbe&cof1n-gl$$jm@(g5&#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,4 +120,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
